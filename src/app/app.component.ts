23093af36446f05faab71975dfe706eed1318ffb
zyzzchad <119152524+gautamalish@users.noreply.github.com>
2024-04-18 08:35:50 +0545

    created Create recipe functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Component, ElementRef, ViewChild, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderCompComponent } from './header-comp/header-comp.component';
 // import { BodyComponent } from './body/body.component';
@@ -6,28 +6,74 @@ import {MatIconModule} from '@angular/material/icon';
 // import {AngularFireModule} from '@angular/fire';
 import { CommonModule } from '@angular/common';
 import { BodyComponent } from './body/body.component';
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,HeaderCompComponent,MatIconModule,CommonModule,BodyComponent],
+  imports: [RouterOutlet,HeaderCompComponent,MatIconModule,CommonModule,BodyComponent,FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  
   showAddRecipe: boolean = false;
   title = 'RecipeApp';
-  onShowAddRecipeChange(value:boolean){
-    this.showAddRecipe=value
+  detailsObj:RecipeDetails=new RecipeDetails();
+  recipeList:RecipeDetails[]=[]
+  ngOnInit(): void {
+    const localData=localStorage.getItem("localRecipe")
+    if(localData!=null){
+      this.recipeList=JSON.parse(localData)
+    }
   }
+  @ViewChild("close") closeModal:ElementRef | undefined
   @ViewChild("exampleModal") exampleModal:ElementRef | undefined
   changeModalDisplay(value:string){
+    this.detailsObj=new RecipeDetails();
     if(this.exampleModal){
-      this.exampleModal.nativeElement.classList.add("show")
+      this.exampleModal.nativeElement.style.display="block"
     }
   }
   onCloseClick(){
     if(this.exampleModal){
-      this.exampleModal.nativeElement.classList.remove("show")
+      this.exampleModal.nativeElement.style.display="none"
     }
+  }
+
+  addRecipe(){
+    const isLocalPresent=localStorage.getItem("localRecipe")
+    if(isLocalPresent!=null){
+      const oldArray=JSON.parse(isLocalPresent);
+      oldArray.push(this.detailsObj);
+      this.recipeList=oldArray;
+      localStorage.setItem("localRecipe",JSON.stringify(oldArray) )
+    }
+    else{
+      const newArr=[];
+      newArr.push(this.detailsObj);
+      this.recipeList=newArr;
+      localStorage.setItem("localRecipe",JSON.stringify(newArr) )
+    }
+    if(this.exampleModal){
+    this.exampleModal.nativeElement.style.display="none"
+    }
+  }
+}
+
+export class RecipeDetails{
+  imageUrl:string;
+  title:string;
+  description:string;
+  ingredients:string;
+  instructions:string;
+  nutrition:string;
+
+  constructor(){
+    this.imageUrl="";
+    this.title="";
+    this.description="";
+    this.ingredients="";
+    this.instructions="";
+    this.nutrition="";
   }
 }
