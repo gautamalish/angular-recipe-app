89c3948cd96867bb3b30470f844175b9d85a7a72
zyzzchad <119152524+gautamalish@users.noreply.github.com>
2024-04-18 16:59:21 +0545

    Added crud operations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,7 @@ import {MatIconModule} from '@angular/material/icon';
 import { CommonModule } from '@angular/common';
 import { BodyComponent } from './body/body.component';
 import { FormsModule } from '@angular/forms';
+import { Ng2SearchPipeModule } from 'ng2-search-filter';
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -20,6 +21,8 @@ export class AppComponent implements OnInit{
   title = 'RecipeApp';
   detailsObj:RecipeDetails=new RecipeDetails();
   recipeList:RecipeDetails[]=[]
+  filteredRecipeList: RecipeDetails[] = [];
+  searchText:string="";
   ngOnInit(): void {
     const localData=localStorage.getItem("localRecipe")
     if(localData!=null){
@@ -28,22 +31,32 @@ export class AppComponent implements OnInit{
   }
   @ViewChild("close") closeModal:ElementRef | undefined
   @ViewChild("exampleModal") exampleModal:ElementRef | undefined
-  changeModalDisplay(value:string){
-    this.detailsObj=new RecipeDetails();
+  changeModalDisplay(){
     if(this.exampleModal){
       this.exampleModal.nativeElement.style.display="block"
     }
   }
   onCloseClick(){
+    this.detailsObj=new RecipeDetails();
     if(this.exampleModal){
       this.exampleModal.nativeElement.style.display="none"
     }
   }
+  onEdit(item:RecipeDetails){
+    this.detailsObj=item;
+    this.changeModalDisplay()
+  }
+  // filterRecipes() {
+  //   this.filteredRecipeList = this.recipeList.filter(item =>
+  //     item.title.toLowerCase().includes(this.searchText.toLowerCase())
+  //   );
+  // }
 
   addRecipe(){
     const isLocalPresent=localStorage.getItem("localRecipe")
     if(isLocalPresent!=null){
       const oldArray=JSON.parse(isLocalPresent);
+      this.detailsObj.id=oldArray.length+1;
       oldArray.push(this.detailsObj);
       this.recipeList=oldArray;
       localStorage.setItem("localRecipe",JSON.stringify(oldArray) )
@@ -51,16 +64,37 @@ export class AppComponent implements OnInit{
     else{
       const newArr=[];
       newArr.push(this.detailsObj);
+      this.detailsObj.id=1;
       this.recipeList=newArr;
       localStorage.setItem("localRecipe",JSON.stringify(newArr) )
     }
-    if(this.exampleModal){
-    this.exampleModal.nativeElement.style.display="none"
+    this.onCloseClick()
+  }
+  onDelete(item:RecipeDetails){
+    const isDelete=confirm("Are you sure you want to delete the recipe?");
+    if(isDelete){
+      const currentRecord=this.recipeList.findIndex(m=>m.id===item.id);
+      this.recipeList.splice(currentRecord,1);
+      localStorage.setItem("localRecipe",JSON.stringify(this.recipeList) )
     }
+  }
+  updateRecipe(){
+    const currentRecord=this.recipeList.find(m=>m.id===this.detailsObj.id);
+    if(currentRecord!=undefined){
+      currentRecord.imageUrl=this.detailsObj.imageUrl;
+      currentRecord.title=this.detailsObj.title;
+      currentRecord.description=this.detailsObj.description;
+      currentRecord.ingredients=this.detailsObj.ingredients;
+      currentRecord.instructions=this.detailsObj.instructions;
+      currentRecord.nutrition=this.detailsObj.nutrition;
+    }
+    localStorage.setItem("localRecipe",JSON.stringify(this.recipeList) )
+    this.onCloseClick()
   }
 }
 
 export class RecipeDetails{
+  id:number;
   imageUrl:string;
   title:string;
   description:string;
@@ -75,5 +109,6 @@ export class RecipeDetails{
     this.ingredients="";
     this.instructions="";
     this.nutrition="";
+    this.id=0;
   }
 }
